faefe158cacc5656420079502a48d22c77cd1d7f
Moritz Gunz <moritz.gunz@gmail.com>
2018-11-15 10:53:01 +0100

    :bug: Fix invalid auth URL

--- src/model/urls.ts
+++ src/model/urls.ts
@@ -1,5 +1,5 @@
 const BACKEND_URL = 'https://velocity-pwa.netlify.com/backend';
 
-export const API_AUTHENTICATION_URL = `${BACKEND_URL}/api/authentication`;
+export const API_AUTHENTICATION_URL = `${BACKEND_URL}/api/authenticate`;
 export const APP_ALL_STATIONS_URL = `${BACKEND_URL}/app/stations`;
 export const singleStationUrl = (statId: number) => `${APP_ALL_STATIONS_URL}/${statId}`;
