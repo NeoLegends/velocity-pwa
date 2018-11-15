987b5b6f9941aa71c63bbd1845f4165619a5d084
Moritz Gunz <moritz.gunz@gmail.com>
2018-11-15 21:53:22 +0100

    :recycle: Refactor URL building pattern

--- src/model/urls.ts
+++ src/model/urls.ts
@@ -1,9 +1,12 @@
 const BACKEND_URL = 'https://velocity-pwa.netlify.com/backend';
+const API_URL = `${BACKEND_URL}/api`;
+const APP_URL = `${BACKEND_URL}/app`;
 
-export const API_IS_AUTHENTICATED_URL = `${BACKEND_URL}/api/authenticate`;
-export const API_LOGIN_URL = `${BACKEND_URL}/api/authentication`;
-export const API_LOGOUT_URL = `${BACKEND_URL}/api/logout`;
-export const APP_ALL_STATIONS_URL = `${BACKEND_URL}/app/stations`;
+export const API_IS_AUTHENTICATED_URL = `${API_URL}/authenticate`;
+export const API_LOGIN_URL = `${API_URL}/authentication`;
+export const API_LOGOUT_URL = `${API_URL}/logout`;
+
+export const APP_ALL_STATIONS_URL = `${APP_URL}/stations`;
 
 export const singleStationUrl = (statId: number) => `${APP_ALL_STATIONS_URL}/${statId}`;
 export const slotInfoUrl = (statId: number) => `${singleStationUrl(statId)}/slots/full`;
