2d63b3b61518666f9b81d4ed7f0b23eb98505c0f
Moritz Gunz <moritz.gunz@gmail.com>
2018-11-20 21:40:08 +0100

    :sparkles: Register service worker in prod builds

--- src/index.tsx
+++ src/index.tsx
@@ -25,4 +25,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+process.env.NODE_ENV === 'production'
+  ? serviceWorker.register()
+  : serviceWorker.unregister();
