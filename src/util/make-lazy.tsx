5d62597a7b487bb4ae1303836746142bd409d23b
Moritz Gunz <moritz.gunz@gmail.com>
2018-11-20 21:23:40 +0100

    :sparkles: Show sleek loading spinner while loading fragments

--- src/util/make-lazy.tsx
+++ src/util/make-lazy.tsx
@@ -1,13 +1,26 @@
 import { RouteComponentProps } from '@reach/router';
 import React, { Suspense } from 'react';
 
+import './make-lazy.scss';
+
+const LazySpinner = (
+  <div className="spinner-lazy">
+    <div className="lds-ellipsis">
+      <div/>
+      <div/>
+      <div/>
+      <div/>
+    </div>
+  </div>
+);
+
 // Needs to be `function` because of ambiguity with JSX
 // tslint:disable-next-line
 const MakeLazy = function<P>(loader: () => Promise<{ default: React.ComponentType<P> }>) {
   const Lazy = React.lazy(loader);
 
   const LazyWrapper: React.SFC<P & RouteComponentProps> = props => (
-    <Suspense fallback={<span>Loading...</span>}>
+    <Suspense fallback={LazySpinner}>
       <Lazy {...(props as any)}/>
     </Suspense>
   );
