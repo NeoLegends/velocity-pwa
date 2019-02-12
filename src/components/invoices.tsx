e972a761131094ef609d4a90a216ed247e981367
Moritz Gunz <moritz.gunz@gmail.com>
2019-02-12 19:51:06 +0100

    :recycle: Rewrite invoices page to use hooks

--- src/components/invoices.tsx
+++ src/components/invoices.tsx
@@ -1,91 +1,62 @@
-import React from 'react';
-import { toast } from 'react-toastify';
+import classNames from 'classnames';
+import React, { useContext } from 'react';
 
-import { Invoice } from '../model';
-import { getAllInvoices } from '../model/invoices';
+import { useInvoices } from '../hooks/invoices';
 import { LanguageContext } from '../resources/language';
 
 import './invoices.scss';
 
-interface InvoicesState {
-  invoices: Invoice[];
+interface InvoicesProps {
+  className?: string;
 }
 
-const InvoicesBody: React.FC<InvoicesState> = ({ invoices }) => (
-  <LanguageContext.Consumer>
-    {({ BILL }) => (
-      <div className="invoices box-list">
-        {!invoices.length && (
-          <div className="note info">
-            {BILL.ALERT.NO_INVOICES}
-          </div>
-        )}
+const Invoices: React.FC<InvoicesProps> = ({ className }) => {
+  const { BILL } = useContext(LanguageContext);
+  const invoices = useInvoices();
 
-        {invoices.length && (
-          <div className="box">
-            <h2>{BILL.HEADLINE}</h2>
+  return (
+    <div className={classNames('invoices box-list', className)}>
+      {!invoices.length && (
+        <div className="note info">
+          {BILL.ALERT.NO_INVOICES}
+        </div>
+      )}
 
-            <table>
-              <thead>
-                <tr>
-                  <th>{BILL.HEAD.JAHR}</th>
-                  <th>{BILL.HEAD.MONAT}</th>
-                  <th>{BILL.HEAD.SUM}</th>
-                  <th>Download</th>
-                </tr>
-              </thead>
-              <tbody>
-                {invoices.map(inv => {
-                  const urlParts = inv.url.split('/');
-                  const invName = urlParts[urlParts.length - 1];
+      {invoices.length && (
+        <div className="box">
+          <h2>{BILL.HEADLINE}</h2>
 
-                  return (
-                    <tr key={inv.url}>
-                      <td>{inv.year}</td>
-                      <td>{inv.month}</td>
-                      <td>{inv.sum.toEuro()}</td>
-                      <td>
-                        <a href={inv.url} target="_blank">{invName}</a>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        )}
-      </div>
-    )}
-  </LanguageContext.Consumer>
-);
+          <table>
+            <thead>
+              <tr>
+                <th>{BILL.HEAD.JAHR}</th>
+                <th>{BILL.HEAD.MONAT}</th>
+                <th>{BILL.HEAD.SUM}</th>
+                <th>Download</th>
+              </tr>
+            </thead>
+            <tbody>
+              {invoices.map(inv => {
+                const urlParts = inv.url.split('/');
+                const invName = urlParts[urlParts.length - 1];
 
-class Invoices extends React.Component<{}, InvoicesState> {
-  static contextType = LanguageContext;
-
-  context!: React.ContextType<typeof LanguageContext>;
-  state = {
-    invoices: [],
-  };
-
-  componentDidMount() {
-    this.fetchInvoices();
-  }
-
-  render() {
-    return <InvoicesBody {...this.state}/>;
-  }
-
-  private async fetchInvoices() {
-    try {
-      const invoices = await getAllInvoices();
-      this.setState({ invoices });
-    } catch (err) {
-      toast(
-        this.context.BILL.ALERT.FETCH_INVOICE_FAILURE,
-        { type: 'error' },
-      );
-    }
-  }
-}
+                return (
+                  <tr key={inv.url}>
+                    <td>{inv.year}</td>
+                    <td>{inv.month}</td>
+                    <td>{inv.sum.toEuro()}</td>
+                    <td>
+                      <a href={inv.url} target="_blank">{invName}</a>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Invoices;
---
+++ src/hooks/invoices.ts
@@ -0,0 +1,27 @@
+import { useContext, useDebugValue, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import { Invoice } from '../model';
+import { getAllInvoices } from '../model/invoices';
+import { LanguageContext } from '../resources/language';
+
+export const useInvoices = () => {
+  const { BILL } = useContext(LanguageContext);
+  const [invoices, setInvoices] = useState<Invoice[]>([]);
+
+  useDebugValue(invoices);
+
+  useEffect(() => {
+    getAllInvoices()
+      .then(setInvoices)
+      .catch(err => {
+        console.error("Error while loading the invoices:", err);
+        toast(
+          BILL.ALERT.FETCH_INVOICE_FAILURE,
+          { type: 'error' },
+        );
+      });
+  }, [BILL]);
+
+  return invoices;
+};
