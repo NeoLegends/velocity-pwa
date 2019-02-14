cc7f2bf71e908363ccfb16b03c4dfd3408d3d63a
Moritz Gunz <moritz.gunz@gmail.com>
2019-02-14 19:17:31 +0100

    :recycle: Rewrite bike-map to use hooks

--- src/components/map/bike-map.tsx
+++ src/components/map/bike-map.tsx
@@ -1,19 +1,13 @@
-import { navigate, RouteComponentProps } from '@reach/router';
+import { navigate } from '@reach/router';
 import { icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import { toast } from 'react-toastify';
 
-import { Slots, Station, StationWithAddress } from '../../model';
-import {
-  getAllStations,
-  getSingleStation,
-  getSlotInfo,
-  rentBike,
-  reserveBike,
-} from '../../model/stations';
-import { hasCurrentBooking } from '../../model/transaction';
+import { useCachedViewport, useOpenableStation } from '../../hooks/map';
+import { useStations } from '../../hooks/stations';
+import { rentBike, reserveBike } from '../../model/stations';
 import { LanguageContext } from '../../resources/language';
 import logo from '../../resources/logo.png';
 
@@ -21,209 +15,86 @@ import './bike-map.scss';
 import StationPopup from './station-popup';
 
 interface BikeMapProps {
+  className?: string;
   isLoggedIn: boolean;
 }
 
-interface BikeMapState {
-  hasBooking: boolean;
-  initialMapLocation: [number, number];
-  initialMapZoom: number;
-  stationOpened: {
-    slots: Slots;
-    station: StationWithAddress;
-  } | null;
-  stations: Station[];
-}
-
-interface BikeMapBodyProps extends BikeMapProps, BikeMapState {
-  onClosePopup: () => void;
-  onOpenStationPopup: (stationId: number) => void;
-  onRent: (pin: string, stationId: number, slotId: number) => void;
-  onReserve: (stationId: number) => void;
-  onViewportChange: (viewport: { center: [number, number], zoom: number }) => void;
-}
-
-const LOCALSTORAGE_STATIONS_KEY = 'velocity/stations';
-const STORAGE_VIEWPORT_KEY = 'velocity/viewport';
-
-const aachenLatLng: [number, number] = [50.77403035497566, 6.084194183349609];
 const stationIcon = icon({
   iconUrl: logo,
   iconSize: [25.3, 29.37],
 });
 
-const BikeMapBody: React.SFC<BikeMapBodyProps> = ({
-  hasBooking,
-  initialMapLocation,
-  initialMapZoom,
-  isLoggedIn,
-  stationOpened,
-  stations,
+const BikeMap: React.SFC<BikeMapProps> = ({ className, isLoggedIn }) => {
+  const { MAP } = useContext(LanguageContext);
 
-  onClosePopup,
-  onOpenStationPopup,
-  onRent,
-  onReserve,
-  onViewportChange,
-}) => (
-  <Map
-    center={initialMapLocation}
-    zoom={initialMapZoom}
-    maxZoom={17}
-    onViewportChanged={onViewportChange}
-  >
-    <TileLayer
-      attribution={'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
-      detectRetina={true}
-      maxZoom={18}
-      url="https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
-    />
+  const [viewport, handleViewportChange] = useCachedViewport();
+  const [
+    openedStation,
+    handleOpenStation,
+    handleCloseStation,
+  ] = useOpenableStation();
+  const [stations] = useStations();
 
-    {stations.map(station => (
-      <Marker
-        icon={stationIcon}
-        key={station.stationId}
-        position={[station.locationLatitude, station.locationLongitude]}
-      >
-        <StationPopup
-          detail={
-            stationOpened && stationOpened.station.stationId === station.stationId
-              ? stationOpened
-              : null
-          }
-          hasBooking={hasBooking}
-          isLoggedIn={isLoggedIn}
-          station={station}
-          onClose={onClosePopup}
-          onOpen={onOpenStationPopup}
-          onRent={onRent}
-          onReserve={onReserve}
-        />
-      </Marker>
-    ))}
-  </Map>
-);
-
-class BikeMap extends React.Component<
-  RouteComponentProps & BikeMapProps,
-  BikeMapState
-> {
-  static contextType = LanguageContext;
-
-  private static storage: Storage =
-    /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
-      ? localStorage
-      : sessionStorage;
-
-  context!: React.ContextType<typeof LanguageContext>;
-  state = {
-    hasBooking: false,
-    initialMapLocation: aachenLatLng,
-    initialMapZoom: 14,
-    stationOpened: null,
-    stations: [],
+  const handleRent = (pin: string, stationId: number, slotId: number) => {
+    rentBike(pin, stationId, slotId)
+      .then(() => toast(
+        MAP.POPUP.RENT_DIALOG.ALERT.DEFAULT_SUCCESS,
+        { type: 'success' },
+      ))
+      .catch(err => {
+        console.error("Error while renting out bike:", err);
+        toast(MAP.POPUP.RENT_DIALOG.ALERT.DEFAULT_ERR, { type: 'error' });
+      });
   };
 
-  componentDidMount() {
-    this.loadPreviousMapViewport();
-    this.loadStations();
-  }
-
-  render() {
-    return (
-      <BikeMapBody
-        {...this.state}
-        isLoggedIn={this.props.isLoggedIn}
-        onClosePopup={this.handleCloseStation}
-        onOpenStationPopup={this.handleOpenStation}
-        onRent={this.handleRent}
-        onReserve={this.handleReserve}
-        onViewportChange={this.handleViewportChange}
-      />
-    );
-  }
-
-  private loadPreviousMapViewport() {
-    const viewport = BikeMap.storage.getItem(STORAGE_VIEWPORT_KEY);
-    if (!viewport) {
-      return;
-    }
-
-    const { center, zoom } = JSON.parse(viewport);
-    this.setState({
-      initialMapLocation: center,
-      initialMapZoom: zoom,
-    });
-  }
-
-  private async loadStations() {
-    try {
-      const lsStations = localStorage.getItem(LOCALSTORAGE_STATIONS_KEY);
-      if (lsStations) {
-        this.setState({ stations: JSON.parse(lsStations) });
-      }
-
-      const stations = await getAllStations();
-      this.setState({ stations });
-
-      localStorage.setItem(LOCALSTORAGE_STATIONS_KEY, JSON.stringify(stations));
-    } catch (err) {
-      toast(
-        this.context.MAP.ALERT.STATION_LOAD,
-        { type: 'error' },
-      );
-    }
-  }
-
-  private handleCloseStation = () => this.setState({ stationOpened: null });
-
-  private handleOpenStation = async (stationId: number) => {
-    try {
-      const [hasBooking, detailedStation, slotInfo] = await Promise.all([
-        hasCurrentBooking(),
-        getSingleStation(stationId),
-        getSlotInfo(stationId),
-      ]);
-
-      if (!detailedStation || !slotInfo) {
-        throw new Error(`Failed fetching station ${stationId}.`);
-      }
-
-      this.setState({
-        hasBooking,
-        stationOpened: {
-          station: detailedStation,
-          slots: slotInfo,
-        },
+  const handleReserve = (stationId: number) => {
+    reserveBike(stationId)
+      .then(() => navigate('/bookings'))
+      .catch(err => {
+        console.error("Error while reserving bike:", err);
+        toast(MAP.POPUP.RENT_DIALOG.ALERT.DEFAULT_ERR, { type: 'error' });
       });
-    } catch (err) {
-      toast(
-        this.context.MAP.ALERT.STATION_DETAILS,
-        { type: 'error' },
-      );
-    }
-  }
+  };
 
-  private handleRent = async (pin: string, stationId: number, slotId: number) => {
-    try {
-      await rentBike(pin, stationId, slotId);
-      navigate('/bookings');
-    } catch (err) {
-      toast(
-        this.context.MAP.POPUP.RENT_DIALOG.ALERT.DEFAULT_ERR,
-        { type: 'error' },
-      );
-    }
-  }
+  return (
+    <Map
+      className={className}
+      center={viewport.center}
+      zoom={viewport.zoom}
+      maxZoom={17}
+      onViewportChanged={handleViewportChange}
+    >
+      <TileLayer
+        attribution={'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
+        detectRetina={true}
+        maxZoom={18}
+        url="https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
+      />
 
-  private handleReserve = async (stationId: number) => {
-    await reserveBike(stationId);
-    navigate('/bookings');
-  }
-
-  private handleViewportChange = viewport => {
-    BikeMap.storage.setItem(STORAGE_VIEWPORT_KEY, JSON.stringify(viewport));
-  }
-}
+      {stations.map(station => (
+        <Marker
+          icon={stationIcon}
+          key={station.stationId}
+          position={[station.locationLatitude, station.locationLongitude]}
+        >
+          <StationPopup
+            detail={
+              openedStation && openedStation.station.stationId === station.stationId
+                ? openedStation
+                : null
+            }
+            hasBooking={openedStation && openedStation.userHasBooking || false}
+            isLoggedIn={isLoggedIn}
+            station={station}
+            onClose={handleCloseStation}
+            onOpen={handleOpenStation}
+            onRent={handleRent}
+            onReserve={handleReserve}
+          />
+        </Marker>
+      ))}
+    </Map>
+  );
+};
 
 export default BikeMap;
---
+++ src/hooks/map.ts
@@ -0,0 +1,89 @@
+import { useCallback, useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+
+import { Slots, StationWithAddress } from '../model';
+import { getSingleStation, getSlotInfo } from '../model/stations';
+import { hasCurrentBooking } from '../model/transaction';
+import { LanguageContext } from '../resources/language';
+
+export interface OpenedStation {
+  slots: Slots;
+  station: StationWithAddress;
+  userHasBooking: boolean;
+}
+
+export interface Viewport {
+  center: [number, number];
+  zoom: number;
+}
+
+const STORAGE_VIEWPORT_KEY = 'velocity/viewport';
+
+const viewportStorage =
+  /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream
+    ? localStorage
+    : sessionStorage;
+
+const defaultViewport: Viewport = {
+  center: [50.77403035497566, 6.084194183349609],
+  zoom: 14,
+};
+
+export const useOpenableStation = () => {
+  const { MAP } = useContext(LanguageContext);
+  const [station, setStation] = useState<OpenedStation | null>(null);
+
+  const handleOpenStation = (stationId: number | null) => {
+    if (stationId === null) {
+      setStation(null);
+      return;
+    }
+
+    Promise.all([
+      hasCurrentBooking(),
+      getSingleStation(stationId),
+      getSlotInfo(stationId),
+    ])
+      .then(([hasBooking, detailedStation, slotInfo]) => {
+        if (!detailedStation || !slotInfo) {
+          throw new Error(`Failed fetching station ${stationId}.`);
+        }
+
+        setStation({
+          slots: slotInfo,
+          station: detailedStation,
+          userHasBooking: hasBooking,
+        });
+      })
+      .catch(err => {
+        console.error("Error while opening station popup:", err);
+
+        toast(MAP.ALERT.STATION_DETAILS, { type: 'error' });
+      });
+  };
+
+  const handleCloseStation = () => setStation(null);
+
+  return [station, handleOpenStation, handleCloseStation] as
+    [OpenedStation | null, (stationId: number) => void, () => void];
+};
+
+export const useCachedViewport = () => {
+  const [viewport, setViewport] = useState<Viewport>(defaultViewport);
+
+  useEffect(() => {
+    const viewport = viewportStorage.getItem(STORAGE_VIEWPORT_KEY);
+    if (!viewport) {
+      return;
+    }
+
+    setViewport(JSON.parse(viewport));
+  }, []);
+
+  const setLsViewport = (v: Viewport) => viewportStorage.setItem(
+    STORAGE_VIEWPORT_KEY,
+    JSON.stringify(v),
+  );
+
+  return [viewport, setLsViewport] as [Viewport, (v: Viewport) => void];
+};
