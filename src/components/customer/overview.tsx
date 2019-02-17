4c092f8da055091016354d5b15c88e66cb73cb5a
Moritz Gunz <moritz.gunz@gmail.com>
2019-02-17 13:42:21 +0100

    :recycle: Refactor customer overview to use hooks

--- src/components/customer/overview.tsx
+++ src/components/customer/overview.tsx
@@ -1,5 +1,5 @@
 import { Link, RouteComponentProps } from '@reach/router';
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Customer, SepaMandate } from '../../model';
 import { LanguageContext } from '../../resources/language';
@@ -12,123 +12,123 @@ interface CustomerBodyProps {
 const Overview: React.FC<CustomerBodyProps & RouteComponentProps> = ({
   customer,
   sepaMandate,
-}) => (
-  <LanguageContext.Consumer>
-    {({ customer: cust, PARTICULARS }) => (
-      <>
-        <div className="box personal">
-          <h2>{PARTICULARS.HEADING}</h2>
+}) => {
+  const { customer: cust, PARTICULARS } = useContext(LanguageContext);
 
-          <div className="wrapper">
-            <table>
-              <tbody>
-                <tr>
-                  <td>{PARTICULARS.TABLE.NAME}</td>
-                  <td>{customer.firstname} {customer.lastname}</td>
-                </tr>
-                <tr>
-                  <td>{PARTICULARS.TABLE.STREET}</td>
-                  <td>{customer.address.streetAndHousenumber}</td>
-                </tr>
-                <tr>
-                  <td>{PARTICULARS.TABLE.ZIP}</td>
-                  <td>{customer.address.zip}</td>
-                </tr>
-                <tr>
-                  <td>{PARTICULARS.TABLE.CITY}</td>
-                  <td>{customer.address.city}</td>
-                </tr>
-                <tr>
-                  <td>{PARTICULARS.TABLE.COUNTRY}</td>
-                  <td>{customer.address.country}</td>
-                </tr>
-                <tr>
-                  <td>{PARTICULARS.TABLE.LOGIN}</td>
-                  <td>{customer.login}</td>
-                </tr>
-                <tr>
-                  <td>{PARTICULARS.TABLE.PHONE}</td>
-                  <td>{customer.phonenumber || "N/A"}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+  return (
+    <>
+      <div className="box personal">
+        <h2>{PARTICULARS.HEADING}</h2>
 
-          <div className="actions">
-            <Link
-              className="btn outline"
-              to="change-address"
-            >
-              {PARTICULARS.TABLE.CHANGE_ADDRESS}
-            </Link>
-
-            <Link
-              className="btn outline"
-              to="change-pin"
-            >
-              {PARTICULARS.PANEL.PIN}
-            </Link>
-
-            <Link
-              className="btn outline"
-              to="change-password"
-            >
-              {PARTICULARS.PANEL.PASSWORD}
-            </Link>
-
-            <Link
-              className="btn outline"
-              to="change-tel"
-            >
-              {PARTICULARS.TABLE.CHANGE_PHONE}
-            </Link>
-          </div>
+        <div className="wrapper">
+          <table>
+            <tbody>
+              <tr>
+                <td>{PARTICULARS.TABLE.NAME}</td>
+                <td>{customer.firstname} {customer.lastname}</td>
+              </tr>
+              <tr>
+                <td>{PARTICULARS.TABLE.STREET}</td>
+                <td>{customer.address.streetAndHousenumber}</td>
+              </tr>
+              <tr>
+                <td>{PARTICULARS.TABLE.ZIP}</td>
+                <td>{customer.address.zip}</td>
+              </tr>
+              <tr>
+                <td>{PARTICULARS.TABLE.CITY}</td>
+                <td>{customer.address.city}</td>
+              </tr>
+              <tr>
+                <td>{PARTICULARS.TABLE.COUNTRY}</td>
+                <td>{customer.address.country}</td>
+              </tr>
+              <tr>
+                <td>{PARTICULARS.TABLE.LOGIN}</td>
+                <td>{customer.login}</td>
+              </tr>
+              <tr>
+                <td>{PARTICULARS.TABLE.PHONE}</td>
+                <td>{customer.phonenumber || "N/A"}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
-        <div className="box banking">
-          <h2>{cust.BANK_DETAILS.BANK_DETAILS}</h2>
+        <div className="actions">
+          <Link
+            className="btn outline"
+            to="change-address"
+          >
+            {PARTICULARS.TABLE.CHANGE_ADDRESS}
+          </Link>
 
-          <div className="wrapper">
-            <table>
-              <tbody>
-                <tr>
-                  <td>{cust.BANK_DETAILS.ACCOUNT_OWNER}</td>
-                  <td>{sepaMandate.firstname} {sepaMandate.lastname}</td>
-                </tr>
-                <tr>
-                  <td>{PARTICULARS.MODAL.BANK_ACC.INPUT.IBAN}</td>
-                  <td>{sepaMandate.iban}</td>
-                </tr>
-                <tr>
-                  <td>
-                    {PARTICULARS.MODAL.BANK_ACC.INPUT.BANK_NAME} & {
-                      PARTICULARS.MODAL.BANK_ACC.INPUT.BIC}
-                  </td>
-                  <td>{sepaMandate.bankName} | {sepaMandate.bic}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <Link
+            className="btn outline"
+            to="change-pin"
+          >
+            {PARTICULARS.PANEL.PIN}
+          </Link>
 
-          <div className="actions">
-            <Link
-              className="btn outline"
-              to="change-bank-details"
-            >
-              {PARTICULARS.PANEL.BANK_ACC}
-            </Link>
+          <Link
+            className="btn outline"
+            to="change-password"
+          >
+            {PARTICULARS.PANEL.PASSWORD}
+          </Link>
 
-            <Link
-              className="btn outline"
-              to="sepa-mandate"
-            >
-              {PARTICULARS.PANEL.SEPA}
-            </Link>
-          </div>
+          <Link
+            className="btn outline"
+            to="change-tel"
+          >
+            {PARTICULARS.TABLE.CHANGE_PHONE}
+          </Link>
         </div>
-      </>
-    )}
-  </LanguageContext.Consumer>
-);
+      </div>
+
+      <div className="box banking">
+        <h2>{cust.BANK_DETAILS.BANK_DETAILS}</h2>
+
+        <div className="wrapper">
+          <table>
+            <tbody>
+              <tr>
+                <td>{cust.BANK_DETAILS.ACCOUNT_OWNER}</td>
+                <td>{sepaMandate.firstname} {sepaMandate.lastname}</td>
+              </tr>
+              <tr>
+                <td>{PARTICULARS.MODAL.BANK_ACC.INPUT.IBAN}</td>
+                <td>{sepaMandate.iban}</td>
+              </tr>
+              <tr>
+                <td>
+                  {PARTICULARS.MODAL.BANK_ACC.INPUT.BANK_NAME} & {
+                    PARTICULARS.MODAL.BANK_ACC.INPUT.BIC}
+                </td>
+                <td>{sepaMandate.bankName} | {sepaMandate.bic}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="actions">
+          <Link
+            className="btn outline"
+            to="change-bank-details"
+          >
+            {PARTICULARS.PANEL.BANK_ACC}
+          </Link>
+
+          <Link
+            className="btn outline"
+            to="sepa-mandate"
+          >
+            {PARTICULARS.PANEL.SEPA}
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Overview;
