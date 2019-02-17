2bbb699229ebb5a6f63bdcc9f927a42457636edf
Moritz Gunz <moritz.gunz@gmail.com>
2019-02-17 13:22:04 +0100

    :recycle: Rewrite change address view to use hooks

--- src/components/customer/change-address.tsx
+++ src/components/customer/change-address.tsx
@@ -1,31 +1,15 @@
-import { RouteComponentProps } from '@reach/router';
+import classNames from 'classnames';
 import countries from 'country-info-list/countries.json';
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 import { Address } from '../../model';
 import { LanguageContext, LanguageIdContext } from '../../resources/language';
 
 interface ChangeAddressProps {
+  className?: string;
+
   onCancel: React.MouseEventHandler;
   onChangeAddress: (newAddress: Address) => void;
-}
-
-interface ChangeAddressState {
-  city: string;
-  country: string;
-  streetAndHousenumber: string;
-  zip: string;
-}
-
-interface ChangeAddressBodyProps extends ChangeAddressState {
-  canSubmit: boolean;
-
-  onCancel: React.MouseEventHandler;
-  onCityChange: React.ChangeEventHandler;
-  onCountryChange: React.ChangeEventHandler;
-  onStreetChange: React.ChangeEventHandler;
-  onSubmit: React.FormEventHandler;
-  onZipChange: React.ChangeEventHandler;
 }
 
 const countryListEnglish = countries.map(c => (
@@ -51,139 +35,94 @@ const alpha3ToCountryName: Record<string, string> = countries.reduce((acc, c) =>
   return acc;
 }, {});
 
-const ChangeAddressBody: React.FC<ChangeAddressBodyProps> = ({
-  canSubmit,
-  city,
-  country,
-  streetAndHousenumber,
-  zip,
+const ChangeAddress: React.FC<ChangeAddressProps> = ({
+  className,
 
   onCancel,
-  onCityChange,
-  onCountryChange,
-  onStreetChange,
-  onSubmit,
-  onZipChange,
-}) => (
-  <LanguageContext.Consumer>
-    {({ PARTICULARS }) => (
-      <LanguageIdContext.Consumer>
-        {langId => (
-          <form className="change-address box" onSubmit={onSubmit}>
-            <h2>{PARTICULARS.MODAL.ADDRESS.TITLE}</h2>
+  onChangeAddress,
+}) => {
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [streetAndHousenumber, setStreetAndHousenumber] = useState('');
+  const [zip, setZip] = useState('');
 
-            <div className="wrapper">
-              <input
-                className="input outline"
-                placeholder={PARTICULARS.MODAL.ADDRESS.INPUT.STREET_AND_HOUSENUMBER}
-                type="text"
-                onChange={onStreetChange}
-                value={streetAndHousenumber}
-              />
-              <input
-                className="input outline"
-                placeholder={PARTICULARS.MODAL.ADDRESS.INPUT.ZIP}
-                type="text"
-                onChange={onZipChange}
-                value={zip}
-              />
-              <input
-                className="input outline"
-                placeholder={PARTICULARS.MODAL.ADDRESS.INPUT.CITY}
-                type="text"
-                onChange={onCityChange}
-                value={city}
-              />
+  const { PARTICULARS } = useContext(LanguageContext);
+  const langId = useContext(LanguageIdContext);
 
-              <select
-                className="input outline"
-                onChange={onCountryChange}
-                value={country}
-              >
-                <option value="">-</option>
-                <option value="DEU">
-                  {langId === 'de' ? "Deutschland" : "Germany"}
-                </option>
-                {langId === 'de' ? countryListGerman : countryListEnglish}
-              </select>
-            </div>
-
-            <div className="actions">
-              <button
-                type="submit"
-                className="btn outline"
-                disabled={!canSubmit}
-              >
-                {PARTICULARS.MODAL.ADDRESS.BUTTON.SUBMIT}
-              </button>
-
-              <button
-                className="btn outline"
-                onClick={onCancel}
-              >
-                {PARTICULARS.MODAL.ADDRESS.BUTTON.CANCEL}
-              </button>
-            </div>
-          </form>
-        )}
-      </LanguageIdContext.Consumer>
-    )}
-  </LanguageContext.Consumer>
-);
-
-class ChangeAddress extends React.Component<
-  ChangeAddressProps & RouteComponentProps,
-  ChangeAddressState
-> {
-  state = {
-    city: '',
-    country: '',
-    streetAndHousenumber: '',
-    zip: '',
-  };
-
-  render() {
-    const canSubmit = Boolean(
-      this.state.city &&
-      this.state.country &&
-      this.state.streetAndHousenumber &&
-      this.state.zip,
-    );
-
-    return (
-      <ChangeAddressBody
-        {...this.state}
-        canSubmit={canSubmit}
-        onCancel={this.props.onCancel}
-        onCityChange={this.handleCityChange}
-        onCountryChange={this.handleCountryChange}
-        onStreetChange={this.handleStreetChange}
-        onSubmit={this.handleSubmit}
-        onZipChange={this.handleZipChange}
-      />
-    );
-  }
-
-  private handleCityChange = (ev: React.ChangeEvent<HTMLInputElement>) =>
-    this.setState({ city: ev.target.value })
-
-  private handleCountryChange = (ev: React.ChangeEvent<HTMLSelectElement>) =>
-    this.setState({ country: ev.target.value })
-
-  private handleStreetChange = (ev: React.ChangeEvent<HTMLInputElement>) =>
-    this.setState({ streetAndHousenumber: ev.target.value })
-
-  private handleZipChange = (ev: React.ChangeEvent<HTMLInputElement>) =>
-    this.setState({ zip: ev.target.value })
-
-  private handleSubmit = (ev: React.FormEvent) => {
+  const handleSubmit = (ev: React.FormEvent) => {
     ev.preventDefault();
 
-    this.props.onChangeAddress({
-      ...this.state,
-      country: alpha3ToCountryName[this.state.country],
+    onChangeAddress({
+      city,
+      country: alpha3ToCountryName[country],
+      streetAndHousenumber,
+      zip,
     });
-  }
-}
+  };
+
+  const canSubmit = Boolean(city && country && streetAndHousenumber && zip);
+
+  return (
+    <form
+      className={classNames('change-address box', className)}
+      onSubmit={handleSubmit}
+    >
+      <h2>{PARTICULARS.MODAL.ADDRESS.TITLE}</h2>
+
+      <div className="wrapper">
+        <input
+          className="input outline"
+          placeholder={PARTICULARS.MODAL.ADDRESS.INPUT.STREET_AND_HOUSENUMBER}
+          type="text"
+          onChange={ev => setStreetAndHousenumber(ev.target.value)}
+          value={streetAndHousenumber}
+        />
+        <input
+          className="input outline"
+          placeholder={PARTICULARS.MODAL.ADDRESS.INPUT.ZIP}
+          type="text"
+          onChange={ev => setZip(ev.target.value)}
+          value={zip}
+        />
+        <input
+          className="input outline"
+          placeholder={PARTICULARS.MODAL.ADDRESS.INPUT.CITY}
+          type="text"
+          onChange={ev => setCity(ev.target.value)}
+          value={city}
+        />
+
+        <select
+          className="input outline"
+          onChange={ev => setCountry(ev.target.value)}
+          value={country}
+        >
+          <option value="">-</option>
+          <option value="DEU">
+            {langId === 'de' ? "Deutschland" : "Germany"}
+          </option>
+          {langId === 'de' ? countryListGerman : countryListEnglish}
+        </select>
+      </div>
+
+      <div className="actions">
+        <button
+          type="submit"
+          className="btn outline"
+          disabled={!canSubmit}
+        >
+          {PARTICULARS.MODAL.ADDRESS.BUTTON.SUBMIT}
+        </button>
+
+        <button
+          className="btn outline"
+          onClick={onCancel}
+        >
+          {PARTICULARS.MODAL.ADDRESS.BUTTON.CANCEL}
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default ChangeAddress;
