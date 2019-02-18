5cb06d3b46b55158751994c43f85c21ed0ea198d
Moritz Gunz <moritz.gunz@gmail.com>
2019-02-18 10:34:26 +0100

    :recycle: Rewrite customer page to use hooks

--- src/components/customer/customer.tsx
+++ src/components/customer/customer.tsx
@@ -1,5 +1,6 @@
 import { Router } from '@reach/router';
-import React from 'react';
+import classNames from 'classnames';
+import React, { useEffect, useState } from 'react';
 
 import { Address, Customer, SepaMandate } from '../../model';
 import {
@@ -15,9 +16,8 @@ import makeLazy from '../../util/make-lazy';
 import './customer.scss';
 import Overview from './overview';
 
-interface CustomerState {
-  customer: Customer | null;
-  sepaMandate: SepaMandate | null;
+interface CustomerViewProps {
+  className?: string;
 }
 
 const ChangeAddress = makeLazy(() => import('./change-address'));
@@ -27,102 +27,96 @@ const ChangePin = makeLazy(() => import('./change-pin'));
 const ChangeTel = makeLazy(() => import('./change-tel'));
 const SepaMandateView = makeLazy(() => import('./sepa-mandate'));
 
-class CustomerView extends React.Component<{}, CustomerState> {
-  state = {
-    customer: null,
-    sepaMandate: null,
+const CustomerView: React.FC<CustomerViewProps> = ({ className }) => {
+  const [customer, setCustomer] = useState<Customer | null>(null);
+  const [sepaMandate, setSepaMandate] = useState<SepaMandate | null>(null);
+
+  const fetchData = () => Promise.all([
+    getCustomer(),
+    getSepaInfo(),
+  ])
+    .then(([customer, sepaMandate]) => {
+      setCustomer(customer);
+      setSepaMandate(sepaMandate);
+    });
+
+  useEffect(() => { fetchData(); }, []);
+
+  if (!customer || !sepaMandate) {
+    return null;
+  }
+
+  const goToOverview = () => history.back();
+
+  const handleCancel = (ev: React.MouseEvent) => {
+    ev.preventDefault();
+    goToOverview();
   };
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  render() {
-    if (!this.state.customer || !this.state.sepaMandate) {
-      return null;
-    }
-
-    return (
-      <Router className="customer-info box-list">
-        <Overview
-          path="/"
-          customer={this.state.customer!}
-          sepaMandate={this.state.sepaMandate!}
-        />
-        <ChangeAddress
-          path="change-address"
-          onCancel={this.handleCancel}
-          onChangeAddress={this.handleChangeAddress}
-        />
-        <ChangeBankDetails
-          path="change-bank-details"
-          onCancel={this.handleCancel}
-          onChangeBankDetails={null!}
-        />
-        <ChangePassword
-          path="change-password"
-          onCancel={this.handleCancel}
-          onRequestPasswordEmail={this.handleRequestPwEmail}
-        />
-        <ChangePin
-          path="change-pin"
-          onCancel={this.handleCancel}
-          onChangePin={this.handleChangePin}
-        />
-        <ChangeTel
-          path="change-tel"
-          onCancel={this.handleCancel}
-          onChangeTel={this.handleChangeTel}
-        />
-        {this.state.sepaMandate && (
-          <SepaMandateView
-            path="sepa-mandate"
-            onCancel={this.handleCancel}
-            mandate={this.state.sepaMandate!}
-          />
-        )}
-      </Router>
-    );
-  }
-
-  private async fetchData() {
-    const [customer, sepaMandate] = await Promise.all([
-      getCustomer(),
-      getSepaInfo(),
-    ]);
-    this.setState({ customer, sepaMandate });
-  }
-
-  private goToOverview() {
-    history.back();
-  }
-
-  private handleCancel = (ev: React.MouseEvent) => {
-    ev.preventDefault();
-    this.goToOverview();
-  }
-
-  private handleChangeAddress = async (addr: Address) => {
+  const handleChangeAddress = async (addr: Address) => {
     await changeAddress(addr);
-    await this.fetchData();
-    this.goToOverview();
-  }
+    await fetchData();
+    goToOverview();
+  };
 
-  private handleChangePin = async (pin: string, pw: string) => {
+  const handleChangePin = async (pin: string, pw: string) => {
     await changePin(pin, pw);
-    this.goToOverview();
-  }
+    await fetchData();
+    goToOverview();
+  };
 
-  private handleChangeTel = async (tel: string) => {
+  const handleChangeTel = async (tel: string) => {
     await changeTel(tel.trim());
-    this.goToOverview();
-  }
+    await fetchData();
+    goToOverview();
+  };
 
-  private handleRequestPwEmail = async () => {
-    const cust: Customer | null = this.state.customer;
-    await requestPasswordResetEmail(cust!.login);
-    this.goToOverview();
-  }
-}
+  const handleRequestPwEmail = async () => {
+    await requestPasswordResetEmail(customer.login);
+    goToOverview();
+  };
+
+  return (
+    <Router className={classNames('customer-info box-list', className)}>
+      <Overview
+        path="/"
+        customer={customer}
+        sepaMandate={sepaMandate}
+      />
+      <ChangeAddress
+        path="change-address"
+        onCancel={handleCancel}
+        onChangeAddress={handleChangeAddress}
+      />
+      <ChangeBankDetails
+        path="change-bank-details"
+        onCancel={handleCancel}
+        onChangeBankDetails={null!}
+      />
+      <ChangePassword
+        path="change-password"
+        onCancel={handleCancel}
+        onRequestPasswordEmail={handleRequestPwEmail}
+      />
+      <ChangePin
+        path="change-pin"
+        onCancel={handleCancel}
+        onChangePin={handleChangePin}
+      />
+      <ChangeTel
+        path="change-tel"
+        onCancel={handleCancel}
+        onChangeTel={handleChangeTel}
+      />
+      {sepaMandate && (
+        <SepaMandateView
+          path="sepa-mandate"
+          onCancel={handleCancel}
+          mandate={sepaMandate!}
+        />
+      )}
+    </Router>
+  );
+};
 
 export default CustomerView;
