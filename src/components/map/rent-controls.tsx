72c092ae35297a92e40228f0810ffee169e08c91
Moritz Gunz <moritz.gunz@gmail.com>
2019-03-16 17:50:24 +0100

    :recycle: Extract rent controls into separate file

---
+++ src/components/map/rent-controls.tsx
@@ -0,0 +1,118 @@
+import classNames from 'classnames';
+import React, { useCallback, useContext } from 'react';
+
+import { useFormField } from '../../hooks/form';
+import { OpenedStation } from '../../hooks/map';
+import { useSavedPin } from '../../hooks/pin';
+import { Booking, Slot, Station } from '../../model';
+import { LanguageContext } from '../../resources/language';
+
+import './rent-controls.scss';
+import Slider from './slider';
+
+interface RentControlsProps {
+  booking: Booking | null;
+  className?: string;
+  openedStation: OpenedStation;
+  selectedSlot: Slot | null;
+  stations: Station[];
+
+  onBookBike: React.MouseEventHandler;
+  onCancelBooking: React.MouseEventHandler;
+  onRentBike: (pin: string) => void;
+}
+
+const RentControls: React.FC<RentControlsProps> = ({
+  booking,
+  className,
+  openedStation,
+  selectedSlot,
+  stations,
+
+  onBookBike,
+  onCancelBooking,
+  onRentBike,
+}) => {
+  const [pin, setPin] = useSavedPin();
+  const [pinInput, handlePinChange] = useFormField('');
+
+  const handleSubmitPin = useCallback(
+    (ev: React.FormEvent) => {
+      ev.preventDefault();
+      setPin(pinInput);
+    },
+    [pinInput],
+  );
+
+  const { map, BUCHUNGEN } = useContext(LanguageContext);
+
+  const canRentBike =
+    openedStation.station.state === 'OPERATIVE' &&
+    openedStation.slots.stationSlots.some(s => s.isOccupied);
+  const bookedStation =
+    booking &&
+    stations.find(station => station.stationId === booking.stationId);
+  const isOpenedStationBooked =
+    booking && booking.stationId === openedStation.station.stationId;
+
+  return pin ? (
+    <div className={classNames('rent-controls', className)}>
+      <Slider
+        background={() => (
+          <div className="slider-content column">
+            <span>
+              {selectedSlot
+                ? `${map.RENT.SLIDE_FOR_BIKE_NO1} ${
+                    selectedSlot.stationSlotPosition
+                  } ${map.RENT.SLIDE_FOR_BIKE_NO2}`
+                : map.RENT.SLIDE_FOR_BEST_BIKE}
+            </span>
+          </div>
+        )}
+        disabled={!canRentBike}
+        onCompleted={() => onRentBike(pin)}
+      />
+
+      <button
+        className="btn outline book"
+        disabled={!canRentBike && !booking}
+        onClick={!booking ? onBookBike : onCancelBooking}
+      >
+        {!booking
+          ? map.BOOKING.BOOK_BIKE
+          : `${BUCHUNGEN.RESERVIERUNG.BUTTON} ${
+              !isOpenedStationBooked ? `(${bookedStation!.name})` : ''
+            }`}
+      </button>
+    </div>
+  ) : (
+    <form
+      className={classNames('rent-controls', className)}
+      onSubmit={handleSubmitPin}
+    >
+      <p>{map.PIN.CTA}</p>
+
+      <input
+        className="input outline"
+        onChange={handlePinChange}
+        placeholder="PIN"
+        type="tel"
+        value={pinInput}
+      />
+
+      <button
+        className="btn outline"
+        disabled={
+          !pinInput ||
+          isNaN((pinInput as unknown) as number) ||
+          !isFinite((pinInput as unknown) as number)
+        }
+        type="submit"
+      >
+        {map.PIN.ACTION}
+      </button>
+    </form>
+  );
+};
+
+export default RentControls;
--- src/components/map/rent-popup.tsx
+++ src/components/map/rent-popup.tsx
@@ -9,117 +9,15 @@ import React, {
 } from 'react';
 import Measure, { ContentRect } from 'react-measure';
 
-import { useFormField } from '../../hooks/form';
-import { useOpenableStation, OpenedStation } from '../../hooks/map';
-import { useSavedPin } from '../../hooks/pin';
+import { useOpenableStation } from '../../hooks/map';
 import { useBooking } from '../../hooks/stations';
-import { Booking, Slot, Station } from '../../model';
+import { Slot, Station } from '../../model';
 import { LanguageContext } from '../../resources/language';
 import Spinner from '../util/spinner';
 
 import BatteryCharge from './battery-charge';
+import RentControls from './rent-controls';
 import './rent-popup.scss';
-import Slider from './slider';
-
-interface RentControlsProps {
-  booking: Booking | null;
-  openedStation: OpenedStation;
-  selectedSlot: Slot | null;
-  stations: Station[];
-
-  onBookBike: React.MouseEventHandler;
-  onCancelBooking: React.MouseEventHandler;
-  onRentBike: (pin: string) => void;
-}
-
-const RentControls: React.FC<RentControlsProps> = ({
-  booking,
-  openedStation,
-  selectedSlot,
-  stations,
-
-  onBookBike,
-  onCancelBooking,
-  onRentBike,
-}) => {
-  const [pin, setPin] = useSavedPin();
-  const [pinInput, handlePinChange] = useFormField('');
-
-  const handleSubmitPin = useCallback(
-    (ev: React.FormEvent) => {
-      ev.preventDefault();
-      setPin(pinInput);
-    },
-    [pinInput],
-  );
-
-  const { map, BUCHUNGEN } = useContext(LanguageContext);
-
-  const canRentBike =
-    openedStation.station.state === 'OPERATIVE' &&
-    openedStation.slots.stationSlots.some(s => s.isOccupied);
-  const bookedStation =
-    booking &&
-    stations.find(station => station.stationId === booking.stationId);
-  const isOpenedStationBooked =
-    booking && booking.stationId === openedStation.station.stationId;
-
-  return pin ? (
-    <div className="rent-controls">
-      <Slider
-        background={() => (
-          <div className="slider-content column">
-            <span>
-              {selectedSlot
-                ? `${map.RENT.SLIDE_FOR_BIKE_NO1} ${
-                    selectedSlot.stationSlotPosition
-                  } ${map.RENT.SLIDE_FOR_BIKE_NO2}`
-                : map.RENT.SLIDE_FOR_BEST_BIKE}
-            </span>
-          </div>
-        )}
-        disabled={!canRentBike}
-        onCompleted={() => onRentBike(pin)}
-      />
-
-      <button
-        className="btn outline book"
-        disabled={!canRentBike && !booking}
-        onClick={!booking ? onBookBike : onCancelBooking}
-      >
-        {!booking
-          ? map.BOOKING.BOOK_BIKE
-          : `${BUCHUNGEN.RESERVIERUNG.BUTTON} ${
-              !isOpenedStationBooked ? `(${bookedStation!.name})` : ''
-            }`}
-      </button>
-    </div>
-  ) : (
-    <form className="rent-controls" onSubmit={handleSubmitPin}>
-      <p>{map.PIN.CTA}</p>
-
-      <input
-        className="input outline"
-        onChange={handlePinChange}
-        placeholder="PIN"
-        type="tel"
-        value={pinInput}
-      />
-
-      <button
-        className="btn outline"
-        disabled={
-          !pinInput ||
-          isNaN((pinInput as unknown) as number) ||
-          !isFinite((pinInput as unknown) as number)
-        }
-        type="submit"
-      >
-        {map.PIN.ACTION}
-      </button>
-    </form>
-  );
-};
 
 interface RentPopupProps {
   className?: string;
