3d318cd45336b9ebc4c66a3d9b74f4438806f546
Moritz Gunz <moritz.gunz@gmail.com>
2019-03-22 18:35:59 +0100

    :art: Inline event handlers into useCallback

--- src/hooks/authentication.ts
+++ src/hooks/authentication.ts
@@ -16,55 +16,60 @@ export const useLogin = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loginStatusKnown, setStatusKnown] = useState(false);
 
-  const fetchLoginState = () =>
-    checkIsLoggedIn()
-      .then(isIn => {
-        setIsLoggedIn(isIn);
-        setStatusKnown(true);
-      })
-      .catch(err => {
-        console.error('Error while checking for login state:', err);
-        toast(LOGIN.ALERT.NO_SERVER_RESPONSE, { type: 'error' });
-      });
+  const fetchLoginState = useCallback(
+    () =>
+      checkIsLoggedIn()
+        .then(isIn => {
+          setIsLoggedIn(isIn);
+          setStatusKnown(true);
+        })
+        .catch(err => {
+          console.error('Error while checking for login state:', err);
+          toast(LOGIN.ALERT.NO_SERVER_RESPONSE, { type: 'error' });
+        }),
+    [LOGIN],
+  );
 
-  const login = (email: string, pw: string) =>
-    doLogin(email, pw)
-      .then(() => {
-        setIsLoggedIn(true);
-        setStatusKnown(true);
-        return true;
-      })
-      .catch(err => {
-        console.error('Error while logging in:', err);
-        const message =
-          err.status === 401
-            ? LOGIN.ALERT.WRONG_USERDATA
-            : LOGIN.ALERT.NO_SERVER_RESPONSE;
-        toast(message, { type: 'error' });
-        return false;
-      });
+  const login = useCallback(
+    (email: string, pw: string) =>
+      doLogin(email, pw)
+        .then(() => {
+          setIsLoggedIn(true);
+          setStatusKnown(true);
+          return true;
+        })
+        .catch(err => {
+          console.error('Error while logging in:', err);
+          const message =
+            err.status === 401
+              ? LOGIN.ALERT.WRONG_USERDATA
+              : LOGIN.ALERT.NO_SERVER_RESPONSE;
+          toast(message, { type: 'error' });
+          return false;
+        }),
+    [LOGIN],
+  );
 
-  const logout = () =>
-    doLogout()
-      .then(() => {
-        setIsLoggedIn(false);
-        setStatusKnown(true);
-      })
-      .catch(err => {
-        console.error('Error while logging out:', err);
-        toast(LOGIN.ALERT.LOGOUT_ERR, { type: 'error' });
-      });
+  const logout = useCallback(
+    () =>
+      doLogout()
+        .then(() => {
+          setIsLoggedIn(false);
+          setStatusKnown(true);
+        })
+        .catch(err => {
+          console.error('Error while logging out:', err);
+          toast(LOGIN.ALERT.LOGOUT_ERR, { type: 'error' });
+        }),
+    [LOGIN],
+  );
 
-  const fetchLoginStateCallback = useCallback(fetchLoginState, [LOGIN]);
-  const loginCallback = useCallback(login, [LOGIN]);
-  const logoutCallback = useCallback(logout, [LOGIN]);
-
-  useInterval(fetchLoginStateCallback);
+  useInterval(fetchLoginState);
 
   return {
     isLoggedIn,
-    login: loginCallback,
+    login,
     loginStatusKnown,
-    logout: logoutCallback,
+    logout,
   };
 };
