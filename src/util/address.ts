f581d1f6bbf4ceda8ad620fcab5b464cf55ff64d
Moritz Gunz <moritz.gunz@gmail.com>
2019-03-25 16:07:52 +0100

    :art: Simplify expression in template string

--- src/util/address.ts
+++ src/util/address.ts
@@ -4,7 +4,7 @@ import { Address } from '../model';
  * Convert the given address struct to a human-readable address with
  * non-breaking spaces between the relevant parts.
  */
-export const asHumanReadable = (addr: Address) =>
-  `${addr.streetAndHousenumber.replace(/ /g, '\u00a0')}, ${addr.zip}\u00a0${
-    addr.city
-  }`;
+export const asHumanReadable = (addr: Address) => {
+  const sanitizedAddress = addr.streetAndHousenumber.replace(/ /g, '\u00a0');
+  return `${sanitizedAddress} ${addr.zip}\u00a0${addr.city}`;
+};
