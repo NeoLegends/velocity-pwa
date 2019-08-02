6bd20b7d09318913886137fbe2edb728fde90a4d
Moritz Gunz <moritz.gunz@gmail.com>
2019-08-02 12:59:06 +0200

    :lock: Use whitespace regex for more safety

--- src/util/address.ts
+++ src/util/address.ts
@@ -5,6 +5,6 @@ import { Address } from '../model';
  * non-breaking spaces between the relevant parts.
  */
 export const asHumanReadable = (addr: Address) => {
-  const sanitizedAddress = addr.streetAndHousenumber.replace(/ /g, '\u00a0');
+  const sanitizedAddress = addr.streetAndHousenumber.replace(/\s/g, '\u00a0');
   return `${sanitizedAddress} ${addr.zip}\u00a0${addr.city}`;
 };
