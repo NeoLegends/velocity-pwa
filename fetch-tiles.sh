b07e9f391cf26af579714403cab3cd17e7133520
Moritz Gunz <moritz.gunz@gmail.com>
2019-10-09 12:58:17 +0200

    :sparkles: Install osm-tile-downloader from crates.io

--- fetch-tiles.sh
+++ fetch-tiles.sh
@@ -5,12 +5,12 @@ set -e
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain none
 source $HOME/.cargo/env
 
-git clone https://github.com/NeoLegends/osm-tile-downloader
+cargo install --force osm-tile-downloader
 
-(cd osm-tile-downloader && cargo run -- \
+osm-tile-downloader \
   --north 50.811 \
   --east 6.1649 \
   --south 50.7492 \
   --west 6.031 \
   --url https://a.tile.openstreetmap.se/hydda/full/\{z\}/\{x\}/\{y\}.png \
-  --output ../public/tile)
+  --output ../public/tile
