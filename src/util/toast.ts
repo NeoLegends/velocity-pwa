29e9fca3bf6593de4d303af21f94fc37dfb2da03
Gero Gerke <11deutron11@gmail.com>
2020-01-07 00:41:54 +0100

    Update Types used

--- src/util/toast.ts
+++ src/util/toast.ts
@@ -1,10 +1,10 @@
-import { toast as toastifyToast } from 'react-toastify';
+import { toast as toastifyToast, ToastOptions } from 'react-toastify';
 
 /**
  * Displays a toast which cannot be duplicated and
  * continues ticking, even when the window loses focus.
  */
-export const toast = (content: string, options: any = {}) => {
+export const toast = (content: string, options: ToastOptions = {}) => {
   if (!toastifyToast.isActive(content)) {
     return toastifyToast(content, { ...options, ...{ toastId: content } });
   }
