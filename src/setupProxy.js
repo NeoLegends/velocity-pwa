cdcdde7132955446f063a41ebfca778f97c0021d
Gero Gerke <hello@gerogerke.de>
2021-02-28 00:46:45 +0100

    locally proxy all netlify redirects in development

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,4 +8,19 @@ module.exports = function (app) {
       target: "https://cms.velocity-aachen.de/",
     }),
   );
+  app.use(
+    "/invoices",
+    createProxyMiddleware({
+      changeOrigin: true,
+      target: "https://cms.velocity-aachen.de/backend/app/customer/",
+    }),
+  );
+  app.use(
+    "/tile",
+    createProxyMiddleware({
+      changeOrigin: true,
+      pathRewrite: (path, req) => path.replace("/tile", "") + ".png",
+      target: "https://a.tile.openstreetmap.de/",
+    }),
+  );
 };
